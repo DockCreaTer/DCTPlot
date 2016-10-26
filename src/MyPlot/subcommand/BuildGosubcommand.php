af65fb238af38bdcbfaad9238bec3153c960e194
TSRlightda <lightda104530@gmail.com>
2016-10-26 17:48:12 +0800

    Update BuildGosubcommand.php

--- src/MyPlot/subcommand/BuildGosubcommand.php
+++ src/MyPlot/subcommand/BuildGosubcommand.php
@@ -1,6 +1,13 @@
 <?php
-namespace BuildGo;
+namespace MyPlot\subcommand;
 
+use pocketmine\command\CommandSender;
+use pocketmine\level\Level;
+use pocketmine\level\Location;
+use pocketmine\level\Position;
+
+class BuildGoSubCommand extends SubCommand
+ 
 $pos = new Position(15,15,15,$this->getServer()->getLevelByName($this->level));
 $sender->teleport($pos);
  $sender->sendMessage(TextFormat::GREEN.$this->tra("进入成功！"));
